e053cee54d71988a32eb4648d27d1e784765c7e9
Jeff Black <codebeneath@gmail.com>
2018-11-28 22:15:50 -0500

    Add edit capability and default text to Unknown for empty fields

--- src/main/java/org/codebeneath/lyrics/verse/VerseController.java
+++ src/main/java/org/codebeneath/lyrics/verse/VerseController.java
@@ -2,26 +2,27 @@ package org.codebeneath.lyrics.verse;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Metrics;
+import java.util.Optional;
+import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.codebeneath.lyrics.impacted.Impacted;
 import org.codebeneath.lyrics.impacted.ImpactedRepository;
 import org.codebeneath.lyrics.tag.TagRepository;
 import org.codebeneath.lyrics.impacted.ImpactedNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Optional;
-import javax.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Slf4j
 @Controller
 public class VerseController {
     
     private final Counter createdCounter = Metrics.counter("verses.created");
+    private final Counter updatedCounter = Metrics.counter("verses.updated");
 
     private final ImpactedRepository impactedRepo;
     private final VerseRepository verseRepo;
@@ -37,21 +38,41 @@ public class VerseController {
     public String getVerseForm(Model model, @RequestParam Optional<Long> randomVerseId) {
         Verse verseToPopulateWith = new Verse();
         if (randomVerseId.isPresent()) {
-            verseToPopulateWith = verseRepo.findById(randomVerseId.get()).get();
+            Verse sourceVerse = verseRepo.findById(randomVerseId.get()).get();
+            verseToPopulateWith.setSong(sourceVerse.getSong());
+            verseToPopulateWith.setArtist(sourceVerse.getArtist());
+            verseToPopulateWith.setLyrics(sourceVerse.getLyrics());            
         }
         model.addAttribute("verse", verseToPopulateWith);
         return "verseForm";
     }
 
+    @GetMapping("/verse/{id}")
+    public String getVerseForm(Model model, @PathVariable Long id){
+        Optional<Verse> verseToPopulateWith = verseRepo.findById(id);
+        if (verseToPopulateWith.isPresent()) {
+            model.addAttribute("verse", verseToPopulateWith.get());
+            return "verseForm";
+        } else {
+            return "redirect:/";
+        }
+    }
+    
     @PostMapping("/verse")
     public String addVerse(@Valid Verse verse, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "verseForm";
         }
         Impacted impactedUser = getImpactedUser(1L);
+        boolean newVerse = verse.getId() == null; 
         verse.setImpacted(impactedUser);
         verseRepo.save(verse);
-        createdCounter.increment();
+
+        if (newVerse) {
+            createdCounter.increment();
+        } else {
+            updatedCounter.increment();
+        }
         
         return "redirect:/";
     }

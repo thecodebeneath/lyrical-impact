47b0ca610e56ae164eeb1ebafc681286c0b151d5
codebeneath@gmail.com <codebeneath@gmail.com>
2020-03-05 23:16:06 -0600

    Fix test

--- applications/mvc-server/src/test/java/org/codebeneath/lyrics/home/HomeControllerTest.java
+++ applications/mvc-server/src/test/java/org/codebeneath/lyrics/home/HomeControllerTest.java
@@ -56,7 +56,7 @@ public class HomeControllerTest {
     @MockBean
     private AuthenticationSuccessHandler successHandler;
     
-    @Autowired
+    @MockBean
     private LoggingAccessDeniedHandler deniedHandler;
 
     @MockBean

49fe136517bad85c27845e0a0e4f72d26a362d0a
codebeneath@gmail.com <codebeneath@gmail.com>
2020-03-22 14:14:29 -0500

    Update docker compose MVC port from 8443 to 9090. Update keycloak realm import file with 9090 as well.

--- k8s/secrets/create-configmap-keycloak.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# script to create a local configmap.yaml file from a plain text json file.
-#
-kubectl create configmap configmap-keycloak-import --from-file=../../docker/vols/keycloak-lyrical-realm.json --dry-run=true -o yaml | 
-kubectl label -f- --local app=lyrical-impact --dry-run -o yaml  > ../configmap-keycloak-import.yaml
